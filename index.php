0a85900afa68a87d50f99f50cc62e59c534687a1
Gantlekey <Gantlekey@gmail.com>
2020-11-25 13:13:17 +0500

    due to next video 9

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Git Learning</title>
+</head>
+<body>
+    <p>The test here!</p>
+    <h1>hello world</h1>
+    <p>just by saeed</p>
+</body>
+</html>
